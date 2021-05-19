5bba2ef610cc356920b1a5e487b10b1bcd220b6e
vittaldummu <vittalvarun96@gmail.com>
2021-05-19 11:34:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ if (process.env.NODE_ENV !== "production") {
 const port = process.env.PORT || 3031;
 
 server.listen(port, () => {
-  console.log(`Server is running on http://13.234.110.206:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 })
